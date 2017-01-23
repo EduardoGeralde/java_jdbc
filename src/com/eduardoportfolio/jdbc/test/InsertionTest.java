d8d6f6a7a44619c8f4b86a455f28abd60d408ec0
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-01-22 23:21:48 -0200

    comments

--- src/com/eduardoportfolio/jdbc/test/InsertionTest.java
+++ src/com/eduardoportfolio/jdbc/test/InsertionTest.java
@@ -5,6 +5,12 @@ import java.util.Calendar;
 import com.eduardoportfolio.jdbc.dao.ContactDao;
 import com.eduardoportfolio.jdbc.model.Contact;
 
+/**
+ * @author Eduardo Geralde Neto
+ * 
+ * This class tests insertion of the contact in DB through the create() method.
+ */
+
 public class InsertionTest {
 
 	public static void main(String[] args) {
