661663b3043a6ccd6f497d12f951cdfffa906a3b
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-01-22 23:20:10 -0200

    exception improved

--- src/com/eduardoportfolio/jdbc/exception/DAOException.java
+++ src/com/eduardoportfolio/jdbc/exception/DAOException.java
@@ -2,6 +2,12 @@ package com.eduardoportfolio.jdbc.exception;
 
 import java.sql.SQLException;
 
+/**
+ * @author Eduardo Geralde Neto
+ * 
+ * Exception created to use in the methods of our ContactDao Class.
+ */
+
 public class DAOException extends RuntimeException {
 	private static final long serialVersionUID = 1L;
 
